51fd57ed9c4557b04bb6050cb926bcc6f4e86337
rvegas <ricardovegas@gmail.com>
2017-10-11 18:29:10 +0200

    Last minute fix

--- server.py
+++ server.py
@@ -6,19 +6,13 @@ from flask_restless import APIManager
 # Create the Flask application and the Flask-SQLAlchemy object.
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./test.db'
 db = SQLAlchemy(app)
-
-
-trades = db.Table('trades',
-        db.Column('coin_id', db.Integer, db.ForeignKey('coin.id')),
-        db.Column('exc_id', db.Integer, db.ForeignKey('exchange.id'))
-)
 
 
 class Coin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(3), primary_key=True)
+    code = db.Column(db.String(3))
     description = db.Column(db.Text)
     created = db.Column(db.DateTime, default=datetime.datetime.now)
 
@@ -27,7 +21,6 @@ class Exchange(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.Text)
     created = db.Column(db.DateTime, default=datetime.datetime.now)
-    coins = db.relationship('Coin', secondary=trades, backref=db.backref('traded_at', lazy='dynamic'))
 
 
 # Create the database tables.
